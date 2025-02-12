c8f97949eb5708bd926a134d7f73a6f719395946
newrhyme <ksh000624@naver.com>
2025-02-12 17:18:30 +0900

    log 출력

--- src/main/java/com/study/Board/config/WebMvcConfig.java
+++ src/main/java/com/study/Board/config/WebMvcConfig.java
@@ -1,4 +1,21 @@
 package com.study.Board.config;
 
-public class WebMvcConfigurer {
+import lombok.extern.slf4j.Slf4j;
+
+import com.study.intercpetor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("LoggerIntercptor 등록됐다");
+
+        registry.addInterceptor(new LoggerInterceptor())
+                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
 }
--- src/main/java/com/study/intercpetor/LoggerInterceptor.java
+++ src/main/java/com/study/intercpetor/LoggerInterceptor.java
@@ -9,21 +9,21 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
-public class LoggerIntercpetor implements HandlerInterceptor {
+public class LoggerInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("=============================================");
         log.debug("=================  시     작  =================");
         log.debug("Request URI ===>" + request.getRequestURI());
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+       // return HandlerInterceptor.super.preHandle(request, response, handler);
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request,HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         log.debug("=================  종     료  =================");
         log.debug("=============================================");
-        log.debug("Request URI ===>" + request.getRequestURI());
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 }
